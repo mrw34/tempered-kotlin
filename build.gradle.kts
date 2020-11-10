6d41213430dd8116356da937025d081b16b417c7
Mark Woodbridge <1101318+mrw34@users.noreply.github.com>
2020-11-10 14:50:11 +0000

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ application {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-netty:1.4.1")
-    implementation("io.ktor:ktor-jackson:1.4.1")
-    implementation("io.ktor:ktor-auth:1.4.1")
-    implementation("org.mongodb:mongodb-driver-sync:4.1.0")
+    implementation("io.ktor:ktor-server-netty:1.4.2")
+    implementation("io.ktor:ktor-jackson:1.4.2")
+    implementation("io.ktor:ktor-auth:1.4.2")
+    implementation("org.mongodb:mongodb-driver-sync:4.1.1")
 }

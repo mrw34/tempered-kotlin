1e794dc51eb9eb520f57b0be46a834c7c29f60f9
Mark Woodbridge <1101318+mrw34@users.noreply.github.com>
2020-12-22 11:19:46 +0000

    ktor 1.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ application {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-netty:1.4.3")
-    implementation("io.ktor:ktor-jackson:1.4.3")
-    implementation("io.ktor:ktor-auth:1.4.3")
+    implementation("io.ktor:ktor-server-netty:1.5.0")
+    implementation("io.ktor:ktor-jackson:1.5.0")
+    implementation("io.ktor:ktor-auth:1.5.0")
     implementation("org.mongodb:mongodb-driver-sync:4.1.1")
 }
